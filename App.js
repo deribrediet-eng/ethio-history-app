09c4279fd50f656c15b87f7477a862408f48aee3
deribrediet-eng <deribrediet@gmail.com>
2026-02-17 17:54:07 -0800

    I changed subtitle color to #e5dab6

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image  } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Ethio-History</Text>
+
+     <Image
+       source={require('./assets/onboarding-hero.png')}
+       style={styles.heroImage}
+       /> 
+      <Text style={styles.subtitle}>
+        Explore Etiopia's Rich History{'\n'}
+        Discover ancient kingdoms, iconic leaders and legendary artifacts.
+      </Text>
+      
+
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +24,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0f0f0f',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 0, 
+    paddingTop: 60,
+  },
+  title: {
+    fontSize: 42,
+    fontWeight: 'bold' ,
+    color: '#FFD700',
+    marginBottom: 32,
+  },
+  heroImage: {
+    width: '95%',
+    height: 350,
+    resizeMode: 'cover',
+    marginBottom: 40,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: 'white',
+    textAlign: 'center',
+    marginHorizontal: 32,
+    marginbottom: 40,
+    lineHeight: 26,
+    flexShrink: 1, 
   },
 });
